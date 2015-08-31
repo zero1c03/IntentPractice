559e33002f1d1c83977e96b3f78afe3ecac6e4f5
zero1c03 <zeo1c03@hotmail.com>
2015-08-31 14:50:37 +0800

    8/31

--- app/src/main/java/com/example/allenchen/intentpractice/MainActivity.java
+++ app/src/main/java/com/example/allenchen/intentpractice/MainActivity.java
@@ -1,11 +1,11 @@
+
 package com.example.allenchen.intentpractice;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.preference.DialogPreference;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -32,18 +32,23 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 AlertDialog.Builder ad = new AlertDialog.Builder((MainActivity.this));
                 ad.setTitle("離開");
-                ad.setMessage("是否離開"));
-                ad.setPositiveButton("是", new DialogInterface.OnClickListener())
+                ad.setMessage("是否離開");
+                ad.setPositiveButton("否", null);
+                ad.setNegativeButton("是", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                });
+
+                ad.show();
             }
+
+
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -58,5 +63,13 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
